88970693cfea264892a9fed62c37a9c32a9b85a9
srujangit123 <srujantmk@gmail.com>
2020-01-13 19:33:01 +0530

    First commit

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -13,7 +13,6 @@ package com.example.android.justjava;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-//import android.support.v7.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.util.Log;
